9b229ba1acf255c47e47e01ce3c0b43a8dfff896
RojoF <rojof83@hotmail.com>
2020-02-21 10:16:21 +0100

    Dar Style al TextView Poner condicionales Poner correctamente margenes

--- app/src/main/java/com/example/stepview2/MainActivity.java
+++ app/src/main/java/com/example/stepview2/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.stepview2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.shuhart.stepview.StepView;
@@ -11,22 +13,25 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txtContenido;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        StepView stepView = (StepView) findViewById(R.id.step_view);
+        txtContenido = (TextView) findViewById(R.id.txtIndicacion);
+        final StepView stepView = (StepView) findViewById(R.id.step_view);
         stepView.getState()
                 .animationType(StepView.ANIMATION_ALL)
                 .steps(new ArrayList<String>() {{
-            add("Paso 1");
-            add("Paso 2");
-            add("Paso 3");
-            add("Paso 4");
-            add("Paso 5");
-            add("Paso 6");
-            add("Paso 7");
-        }})
+                    add("Paso 1");
+                    add("Paso 2");
+                    add("Paso 3");
+                    add("Paso 4");
+                    add("Paso 5");
+                    add("Paso 6");
+                    add("Paso 7");
+                }})
                 .stepsNumber(7)
                 .animationDuration(getResources().getInteger(android.R.integer.config_longAnimTime))
                 .commit();
@@ -34,9 +39,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onStepClick(int step) {
                 Toast.makeText(MainActivity.this, "Step " + step, Toast.LENGTH_LONG).show();
-            }
-        });
 
+
+                if (step == 0) {
+
+                    stepView.getCurrentStep();
+                    stepView.getState()
+                    .selectedTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryDark));
+                    txtContenido.setText(getString(R.string.ruta_uno));
+                }
+
+                if (step == 1) {
+
+                    stepView.getCurrentStep();
+                    txtContenido.setText(getString(R.string.ruta_dos));
+                }
+
+            }
+
+
+        });
 
 
     }
